c950ceeb32a5506d7baf404975e456ff4d71c0ce
AbhishekSharma1102 <abhishekk.ssharma11@gmail.com>
2026-05-28 22:55:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Jenkins CI/CD Pipeline Started");
+
+let a = 10;
+let b = 20;
+
+console.log("Addition:", a + b);
+console.log("Multiplication:", a * b);
+
+console.log("Pipeline Execution Successful");
