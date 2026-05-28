2d3c6c5e5626220a21f79d9e282bb7e2ac438fb7
AbhishekSharma1102 <abhishekk.ssharma11@gmail.com>
2026-05-28 23:17:24 +0530

    update it

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ let b = 20;
 console.log("Addition:", a + b);
 console.log("Multiplication:", a * b);
 console.log("Subtraction:",a-b);
+console.log("Diision:",a/b);
 
 console.log("Pipeline Execution Successful");
